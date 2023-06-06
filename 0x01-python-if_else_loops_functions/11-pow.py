2c418ff7761bfe6c2ccffdac6929ce39ccb9737f
laaouini taha <laaouini.taha@gmail.com>
2023-06-06 15:30:51 -0700

    L.Taha

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def pow(a, b):
+    result = 1
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result /= a
+    return result
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
